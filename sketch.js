a267b12eaf042b6b5d1670a32768eebe65b1010b
WhitehatjrMeenu <75825480+WhitehatjrMeenu@users.noreply.github.com>
2020-12-11 10:56:16 +0530

    Add files via upload
    c24 template

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+   
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+textSize(30);
+fill("green");
+text("This is template",600,200);
+   }
